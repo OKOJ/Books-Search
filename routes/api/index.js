042dc1407cd3b1d4921137b4c59d15b6bdc1df94
OKOJ <aajmd@hotmail.com>
2019-06-23 18:12:29 -0600

    routes and models

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,17 @@
+const path = require("path");
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const savedRoutes = require("./saved");
+const searchRoutes = require("./search");
 
-// Book routes
-router.use("/books", bookRoutes);
+// Saved books routes
+router.use("/saved", savedRoutes);
+//Book search routes
+router.use("/search", searchRoutes);
+
+// Render the html page
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  });
+  
 
 module.exports = router;
